d259ced8777ed5954442922e6afccf542b5ad5de
Róbert Selvek <robert.selvek@gmail.com>
2012-11-06 20:25:55 +0100

    Improved documentation for Config model

--- models/config.go
+++ models/config.go
@@ -5,15 +5,19 @@ import (
 	"appengine/datastore"
 )
 
+//Config stores central configuration in Datastore.
+//There is always only one Config record in Datastore.
 type Config struct {
-	Data []byte
+	Data []byte //Stores JSON that is sent to clients as centralConfig
 }
 
+//GetKey returns key of the Datastore Config record.
 func (c Config) GetKey(ctx appengine.Context) (k *datastore.Key, err error) {
 	k, err = datastore.NewQuery("Config").KeysOnly().Run(ctx).Next(nil)
 	return
 }
 
+//SaveConfig saves data provided into the Config record.
 func SaveConfig(newData []byte, c appengine.Context) (err error) {
 	var key *datastore.Key
 	key, err = datastore.NewQuery("Config").KeysOnly().Run(c).Next(nil)
@@ -31,6 +35,8 @@ func SaveConfig(newData []byte, c appengine.Context) (err error) {
 	return
 }
 
+//GetConfig fetches the Config record from Datastore and
+//returns its data.
 func GetConfig(c appengine.Context) (data []byte, err error) {
 	var conf Config
 	_, err = datastore.NewQuery("Config").Run(c).Next(&conf)
