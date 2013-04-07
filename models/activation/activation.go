d6fd4b887618f964cef094530f5cc3906cea039a
Róbert Selvek <robert.selvek@gmail.com>
2013-04-07 11:10:47 +0200

    Added query functions to activation

--- models/activation/activation.go
+++ models/activation/activation.go
@@ -27,6 +27,43 @@ func Make(t time.Time, p presentation.Presentation, c appengine.Context) (a *Act
 	return
 }
 
+func GetByKey(k string, c appengine.Context) (a *Activation, err error) {
+	dk, err := datastore.DecodeKey(k)
+	if err != nil {
+		return
+	}
+
+	err = datastore.Get(c, dk, a)
+	return
+
+}
+
+func GetForPresentation(p presentation.Presentation, c appengine.Context) (as []*Activation, err error) {
+	as = make([]*Activation, 0)
+	pk, err := datastore.DecodeKey(p.Key)
+	if err != nil {
+		return
+	}
+
+	keys, err := datastore.NewQuery("Activation").Ancestor(pk).GetAll(c, as)
+	if err != nil {
+		return
+	}
+
+	for i, k := range keys {
+		as[i].Key = k.Encode()
+	}
+	return
+}
+
+func GetBeforeTime(t time.Time, c appengine.Context) ([]*Activation, error) {
+	return timeQuery(t, "<", c)
+}
+
+func GetAfterTime(t time.Time, c appengine.Context) ([]*Activation, error) {
+	return timeQuery(t, ">", c)
+}
+
 func (a *Activation) Save(c appengine.Context) (err error) {
 	var k *datastore.Key
 	if a.Key == "" {
@@ -49,6 +86,19 @@ func (a *Activation) Save(c appengine.Context) (err error) {
 
 		_, err = datastore.Put(c, k, a)
 		return
+	}
+	return
+}
+
+func timeQuery(t time.Time, sign string, c appengine.Context) (as []*Activation, err error) {
+	as = make([]*Activation, 0)
+	keys, err := datastore.NewQuery("Activation").Filter("Time "+sign, t).GetAll(c, as)
+	if err != nil {
+		return
+	}
+
+	for i, k := range keys {
+		as[i].Key = k.Encode()
 	}
 	return
 }
