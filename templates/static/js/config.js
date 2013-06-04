eeb9a52b35779fb1c8b9b80e50493fb6d17ee489
Róbert Selvek <robert.selvek@gmail.com>
2013-06-04 16:21:50 +0200

    Improved config UI

--- templates/static/js/config.js
+++ templates/static/js/config.js
@@ -1,4 +1,11 @@
 "use strict"
 $(function() {
 	$(".timeField").timepicker();
+	$("input[name=overrideState]").on("click", function(){
+		if ($("input[name=overrideState]:checked").val() !== "0") {
+			$("input:not(input[name=overrideState]):not(input[type=submit])").attr("disabled", "true");
+		} else {
+			$("input:not(input[name=overrideState]):not(input[type=submit])").removeAttr("disabled");
+		}
+	});
 })
