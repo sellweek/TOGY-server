bfeb58da486e2a8abc1053ee8a1873f06aed9e92
Róbert Selvek <robert.selvek@gmail.com>
2013-10-31 22:09:57 +0100

    Added a migration function

--- controllers/admin/util.go
+++ controllers/admin/util.go
@@ -34,6 +34,14 @@ func Bootstrap(c util.Context) (err error) {
 
 //Migrate migrates Datastore data from a previous version.
 func Migrate(c util.Context) (err error) {
-	fmt.Fprintf(c.W, "There is nothing to migrate in current version.")
+	keys, err := datastore.NewQuery("Action").KeysOnly().GetAll(c.Ac, nil)
+	if err != nil {
+		return err
+	}
+	err = datastore.DeleteMulti(c.Ac, keys)
+	if err != nil {
+		return
+	}
+	fmt.Fprintf(c.W, "Success, %d actions deleted", len(keys))
 	return
 }
