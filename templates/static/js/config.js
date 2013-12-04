9b755a1c05da7422b8107ceaf5226c18496ddaca
Róbert Selvek <robert.selvek@gmail.com>
2013-12-04 21:33:38 +0100

    Fixed a bug in config UI

--- templates/static/js/config.js
+++ templates/static/js/config.js
@@ -6,11 +6,22 @@ $(function() {
 		language: "sk",
 
 	});
-	$("input[name=overrideState]").on("click", function(){
-		if ($("input[name=overrideState]:checked").val() !== "0") {
+
+	var setTimeFieldState = function() {
+    	if ($("input[name=overrideState]:checked").val() !== "0") {
 			$("input:not(input[name=overrideState]):not(input[type=submit])").attr("disabled", "true");
 		} else {
 			$("input:not(input[name=overrideState]):not(input[type=submit])").removeAttr("disabled");
 		}
+    }
+
+	setTimeFieldState();
+
+	$("input[name=overrideState]").on("click", function(){
+		setTimeFieldState();
 	});
+
+	$('.conf-form').on('submit', function() {
+        $('input:not(input[name=overrideState]):not(input[type=submit])').removeAttr('disabled');
+    });
 })
