bac564671d02c00570a74121600a1619c7d32658
Róbert Selvek <robert.selvek@gmail.com>
2014-03-07 16:24:37 +0100

    Converted presentation to use gaemodel

--- models/presentation/presentation.go
+++ models/presentation/presentation.go
@@ -1,4 +1,3 @@
-//Package models provides models used in application.
 package presentation
 
 import (
@@ -6,6 +5,7 @@ import (
 	"appengine/blobstore"
 	"appengine/datastore"
 	"fmt"
+	"github.com/sellweek/gaemodel"
 	"math"
 	"models/action"
 	"time"
@@ -20,27 +20,35 @@ type Presentation struct {
 	FileType    string            //Name that clients should store the presentation under
 	Active      bool              //If true, presentation is distributed to clients. Only one presentation can be active.
 
-	//Used for storage of Datastore key when passing the struct around. Doesn't get saved to DS.
-	//The key is stored as an encoded string
-	Key string `datastore:"-"`
+	Key *datastore.Key `datastore:"-"`
 }
 
-func (p Presentation) GetKey(ctx appengine.Context) (k *datastore.Key, err error) {
-	k, err = datastore.DecodeKey(p.Key)
-	return
+var typ = reflect.TypeOf(Presentation{})
+
+func (p *Presentation) Key() *datastore.Key {
+	return p.key
+}
+
+func (p *Presentation) SetKey(k *datastore.Key) {
+	p.key = k
+}
+
+func (p *Presentation) Kind() string {
+	return "Presentation"
+}
+
+func (_ *Presentation) Ancestor() *datastore.Key {
+	return nil
 }
 
 //GetActive gets the active presentations from the Datastore.
 func GetActive(c appengine.Context) (ps []*Presentation, err error) {
-	ps = make([]*Presentation, 0)
-	keys, err := datastore.NewQuery("Presentation").Filter("Active =", true).GetAll(c, &ps)
+	q := datastore.NewQuery("Presentation").Filter("Active =", true)
+	is, err := gaemodel.MultiQuery(c, typ, "Presentation", q)
 	if err != nil {
 		return
 	}
-
-	for i, p := range ps {
-		p.Key = keys[i].Encode()
-	}
+	ps = is.([]*Presentation)
 	return
 }
 
@@ -65,107 +73,44 @@ func Make(k appengine.BlobKey, fileType, name string, desc []byte, active bool, 
 
 //Save saves presentation to Datastore.
 func (p *Presentation) Save(c appengine.Context) (err error) {
-	var k *datastore.Key
-	if p.Key == "" {
-
-		k, err = datastore.Put(c, datastore.NewIncompleteKey(c, "Presentation", nil), p)
-		if err != nil {
-			return
-		}
-
-		p.Key = k.Encode()
-	} else {
-		k, err = datastore.DecodeKey(p.Key)
-		if err != nil {
-			return
-		}
-
-		if p.Active {
-			action.DeleteFor(p, c)
-		}
-		_, err = datastore.Put(c, k, p)
-		if err != nil {
-			return
-		}
-
-	}
-	return
+	return gaemodel.Save(c, p)
 }
 
 //Delete deletes the presentation record from Datastore and
 //its data file from Blobstore.
 func (p *Presentation) Delete(c appengine.Context) (err error) {
-	if p.Active {
-		err = fmt.Errorf("Active presentation can't be deleted.")
-		return
-	}
-	k, err := datastore.DecodeKey(p.Key)
+	err = gaemodel.Delete(c, p)
 	if err != nil {
 		return
 	}
-
-	err = datastore.Delete(c, k)
-	if err != nil {
-		return
-	}
-	p.Key = ""
 
 	err = blobstore.Delete(c, p.BlobKey)
 
 	return
 }
 
-//GetAll returns a slice of all the presentations in Datastore.
-func GetAll(c appengine.Context) (ps []*Presentation, err error) {
-	ps = make([]*Presentation, 0)
-	keys, err := datastore.NewQuery("Presentation").Order("-Created").GetAll(c, &ps)
-	for i := 0; i < len(ps); i++ {
-		ps[i].Key = keys[i].Encode()
-	}
-	return
-}
-
 //GetListing gets paginated Presentations from Datastore.
 func GetListing(page int, perPage int, c appengine.Context) (ps []*Presentation, err error) {
-	var q *datastore.Query
-	if page == 1 {
-		q = datastore.NewQuery("Presentation").Limit(perPage).Order("-Active").Order("-Created")
-	} else {
-		q = datastore.NewQuery("Presentation").Limit(perPage).Offset(perPage * (page - 1)).Order("-Active").Order("-Created")
-	}
-
-	keys, err := q.GetAll(c, &ps)
+	is, err := gaemodel.GetAll(c, typ, "Presentation", page, perPage)
 	if err != nil {
 		return
 	}
-	for i, p := range ps {
-		p.Key = keys[i].Encode()
-	}
+	ps = is.([]*Presentation)
 	return
 }
 
 //PageCount returns how many pages the Presentations in Datastore would need
 //if there wer perPage presentations listed on a single page
-func PageCount(perPage int, c appengine.Context) (pgs int, err error) {
-	ps, err := datastore.NewQuery("Presentation").Count(c)
-	if err != nil {
-		return
-	}
-	pgs = int(math.Ceil(float64(ps) / float64(perPage)))
-	return
+func PageCount(perPage int, c appengine.Context) (int, error) {
+	return gaemodel.PageCount(c, "Presentation", perPage)
 }
 
 //GetByKey gets the presentation with given key from Datastore and returns a pointer to it.
-func GetByKey(key string, c appengine.Context) (p *Presentation, err error) {
-	k, err := datastore.DecodeKey(key)
+func GetByKey(key *datastore.Key, c appengine.Context) (p *Presentation, err error) {
+	i, err := gaemodel.GetByKey(c, typ, key)
 	if err != nil {
 		return
 	}
-	p = new(Presentation)
-	err = datastore.Get(c, k, p)
-	if err != nil {
-		return
-	}
-	p.Key = key
+	p = i.(*Presentation)
 	return
 }
